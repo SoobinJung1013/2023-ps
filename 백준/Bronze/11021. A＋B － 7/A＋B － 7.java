6cf88e3000313e4ea837e06e13a49677702428ca
Soobin Jung <76704035+SoobinJung1013@users.noreply.github.com>
2023-11-18 13:23:41 +0900

    [Bronze V] Title: A+B - 7, Time: 156 ms, Memory: 16196 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
+//        BufferedWriter writer = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
+        long n = Integer.parseInt(reader.readLine());
+
+        for ( int i = 1; i <= n; i++ ) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = a + b;
+            System.out.println("Case #"+ i + ": " + c);
+//            writer.write(a + b + "\n");
+        }
+//        writer.flush();
+//        writer.close();
+
+    }
+}
