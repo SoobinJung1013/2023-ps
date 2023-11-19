578142f74a96b8529ef687ab3097b5f9eaeb6764
Soobin Jung <76704035+SoobinJung1013@users.noreply.github.com>
2023-11-20 02:43:46 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 200 ms, Memory: 16400 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,33 +1,54 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.IOException;
+
+import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
+
+    public static boolean[] prime = new boolean[1000001];
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
 
-        // 소수 구하기 = 소수 false
-        boolean[] num = new boolean[1000001];
-        num[0] = num[1] = true;
-        for (int i = 2; i * i <= 1000000; i++) {
-            if (!num[i]) {
-                for (int j = i + i; j <= 1000000; j += i) {
-                    num[j] = true;
-                }
+        makePrimeList(1000001);
+
+        for (int i = 0; i < n; i++) {
+             int a = Integer.parseInt(br.readLine());
+             int cnt = goldbach(a);
+             sb.append(cnt).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+    static int goldbach(int a) {
+        int cnt = 0;
+        for (int i = 2; i <= a / 2; i++) {
+            if (!prime[i] && !prime[a - i]) {
+                cnt++;
             }
         }
-
-        while (t-- > 0) {
-            int temp = Integer.parseInt(br.readLine());
-            int ans = 0;
-            for (int j = 2; j <= temp / 2; j++) {
-                if (!num[j] && !num[temp - j]) ans++;
-            }
-            System.out.println(ans);
-        }
+        return cnt;
     }
 
+    public static void makePrimeList(int end) {
+        prime = new boolean[end + 1];
+        if (end <= 1) {
+            return;
+        }
+        prime[0] = prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(end); i++) {
+            if (prime[i]) {
+                continue;
+            }
+            for (int j = i * i; j <= end; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
 }
