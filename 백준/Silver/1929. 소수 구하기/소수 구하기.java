05afcfa64626b56b93574cee3054dc07090e1bb2
Soobin Jung <76704035+SoobinJung1013@users.noreply.github.com>
2023-11-20 02:07:14 +0900

    [Silver III] Title: 소수 구하기, Time: 496 ms, Memory: 18984 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = start; i <= end; i++) {
+            if(isPrime(i)) {
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+    static boolean isPrime(int n) {
+        if(n == 1) return false;
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+}
