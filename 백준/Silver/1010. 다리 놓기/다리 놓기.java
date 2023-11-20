5cfc7454a8f32969c5cdab2a8501fd0cd3c4a537
Soobin Jung <76704035+SoobinJung1013@users.noreply.github.com>
2023-11-21 05:38:52 +0900

    [Silver V] Title: 다리 놓기, Time: 132 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] dp = new int[30][30];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder ans = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            ans.append(combi(b, a)).append("\n");
+        }
+        System.out.println(ans);
+    }
+
+    static int combi(int n, int r) {
+
+        if(dp[n][r] > 0) {
+            return dp[n][r];
+        }
+
+        if(n == r || r == 0) {
+            return dp[n][r] = 1;
+        }
+
+        return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
+    }
+}
