07e629eaca4216d129ed5d03b84b997c148ac186
Soobin Jung <76704035+SoobinJung1013@users.noreply.github.com>
2023-11-21 04:58:30 +0900

    [Bronze IV] Title: 베라의 패션, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                else cnt++;
+            }
+        } 
+
+        System.out.println(cnt);
+    }
+}
