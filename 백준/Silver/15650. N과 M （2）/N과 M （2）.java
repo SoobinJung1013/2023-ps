ff4c84347c16d81c01f6c098b250825e4e73910a
Soobin Jung <76704035+SoobinJung1013@users.noreply.github.com>
2023-11-22 03:33:13 +0900

    [Silver III] Title: N과 M (2), Time: 132 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        combination(1, 0);
+
+        System.out.println(sb);
+    }
+
+    public static void combination(int start, int depth) {
+        if (depth == M) {
+            for (int value : arr) {
+                sb.append(value).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            arr[depth] = i;
+            combination(i + 1, depth + 1);
+        }
+    }
+}
